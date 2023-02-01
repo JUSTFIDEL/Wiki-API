202e73f912eb4e430a2b88e4d2878faa138c4472
JUSTFIDEL <fidenki1429@gmail.com>
2023-02-02 00:39:33 +0100

    Second Commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const _ = require('lodash')
+
+mongoose.set('strictQuery', false);
 
 const app = express();
 
@@ -11,14 +14,54 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect('mongodb://localhost/27017/wikiDB');
+mongoose.connect('mongodb://127.0.0.1:27017/wikiDB');
+// mongoose.connect("mongodb+srv://admin-justfidel:Password123@cluster0.30p6n62.mongodb.net/wikiDB");
 
-const articlesSchema = {
+
+const articleSchema = {
   title: String,
   content: String
 };
 
-const Article = mongoose.model('Article', articlesSchema);
+const Article = mongoose.model('Article', articleSchema);
+
+app.route('articles').get((req, res) => {
+
+  Article.find((err, foundArticles) => {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  })
+})
+
+.post((req, res) => {
+
+  const newArticle = new Article ({
+    title: req.body.title, 
+    content: req.body.content
+  })
+
+  newArticle.save((err) => {
+    if (!err) {
+      res.send('Post successful');
+    } else {
+      res.send(err);
+    }
+  });
+})
+
+.delete((req, res) => {
+
+  Article.deleteMany((err) => {
+    if (!err) {
+      res.send('Post successful DELETED xxx');
+    } else {
+      res.send(err);
+    }
+  })
+});
 
 
 
